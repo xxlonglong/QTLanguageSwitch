e549555e761e99fddc3f0df87b4f16b5470d3c6e
xxlonglong <93241156@qq.com>
2019-10-13 10:39:09 +0800

    项目初始化

---
+++ QTLanguageSwitch/en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="dialog.ui" line="13"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
