90c067d3d5a99ac8f1c3b57c1e4ba3c2db9c56cf
xxlonglong <93241156@qq.com>
2019-10-21 23:22:34 +0800

    工作时保存,已完成语言切换

--- QTLanguageSwitch/dialog.cpp
+++ QTLanguageSwitch/dialog.cpp
@@ -1,6 +1,8 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 #include <QTranslator>
+#include <QFileInfo>
+#include <QMessageBox>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -21,8 +23,16 @@ void Dialog::on_Btn_ToChs_clicked(bool checked)
 
 void Dialog::on_Btn_ToChs_clicked()
 {
+
+    QFileInfo file("./a.txt");
+
+    if(file.exists()==false)
+    {
+        QMessageBox::information(NULL, "Title", "文件不存在",QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+    }
+
     QTranslator m;
-    m.load(":/Chinese.qm");
+    m.load("./Chinese.qm");
     qApp->installTranslator(&m);
     this->ui->retranslateUi(this);
 }
